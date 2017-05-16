f819926c82fb32b4959717b76d03c02bd6f77d96
H2SU <hisu138@gmail.com>
2017-05-16 18:01:29 +0900

    김희수 가위바위보 파일 추가

---
+++ H2SU/rps.py
@@ -0,0 +1,13 @@
+#-*-coding:utf-8-*-
+name = "H2SU" # 플레이어 이름
+def showRSP(mine, others):
+    i = 0
+    if (i==0):
+        i+=1
+        return "paper"
+    elif (i==1):
+        i+=1
+        return "scissors"
+    elif(i==2):
+        i=0
+        return "rock"
